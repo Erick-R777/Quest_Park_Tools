60a449c92dea4dcd0cd167b9df0c4e78cab4164a
Erick-R777 <elsbr300@gmail.com>
2025-05-08 14:25:34 +0200

    changed .ini file

--- Quest_code/src/main.cpp
+++ Quest_code/src/main.cpp
@@ -1,17 +1,26 @@
 #include <Arduino.h>
 
+#define TX  18   // for the wave sensor
+#define RX  11   // for the wave sensor
+
+HardwareSerial WaveSerial(1); // UART 1
 
 
 void setup() 
 {
     Serial.begin(115200);
-    
+    WaveSerial.begin(115200, SERIAL_8N1, RX, TX);
+    Serial.println("ESP32-S3 UART1 ready");
 
 }
 
 void loop() 
 {
+  while (WaveSerial.available()) 
+  {
+    int data = WaveSerial.read();
+    Serial.write(data);
+  }
  
-  
 }
 
