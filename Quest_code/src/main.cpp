fa8c77bf8d977e7031c7cb808586a4c2f86b81be
Erick-R777 <elsbr300@gmail.com>
2025-06-03 18:43:51 +0200

    Add some lib, made sure I'm using an arduino uno. and made the first version of the code for the temp sensor

--- Quest_code/src/main.cpp
+++ Quest_code/src/main.cpp
@@ -1,26 +1,61 @@
-#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 
-#define TX  18   // for the wave sensor
-#define RX  11   // for the wave sensor
-
-HardwareSerial WaveSerial(1); // UART 1
-
+Adafruit_BME280 bme;  // I2C interface
 
 void setup() 
 {
-    Serial.begin(115200);
-    WaveSerial.begin(115200, SERIAL_8N1, RX, TX);
-    Serial.println("ESP32-S3 UART1 ready");
+  Serial.begin(9600);
+  
+  // Initialize BME280 sensor
+  if (!bme.begin(0x76)) 
+  {  // Try 0x77 if 0x76 doesn't work
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);  // Stop here if sensor is not found
+  }
 
+  Serial.println("BME280 Sensor Initialized.");
 }
 
 void loop() 
 {
-  while (WaveSerial.available()) 
-  {
-    int data = WaveSerial.read();
-    Serial.write(data);
-  }
- 
+  float temperature = readTemperature();
+  float humidity = readHumidity();
+  float pressure = readPressure();
+
+  displayReadings(temperature, humidity, pressure);
+
+  delay(2000);  // Update every 2 seconds
+}
+
+// Function to read temperature in Celsius
+float readTemperature() 
+{
+  return bme.readTemperature();
+}
+
+// Function to read humidity in percentage
+float readHumidity() 
+{
+  return bme.readHumidity();
+}
+
+// Function to read pressure in hPa
+float readPressure() 
+{
+  return bme.readPressure() / 100.0F;  // Convert Pa to hPa
+}
+
+// Function to display all sensor readings
+void displayReadings(float temp, float hum, float pres) 
+{
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.print(" °C | Humidity: ");
+  Serial.print(hum);
+  Serial.print(" % | Pressure: ");
+  Serial.print(pres);
+  Serial.println(" hPa");
 }
 
