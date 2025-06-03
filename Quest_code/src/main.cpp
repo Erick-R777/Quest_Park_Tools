dceec8fe79c90450943a7fc3159d3ec4435aa1a1
Erick-R777 <elsbr300@gmail.com>
2025-06-03 14:37:57 +0200

    channged to an Arduino uno, and made the sensor add to an counter each time it detects a person

--- Quest_code/src/main.cpp
+++ Quest_code/src/main.cpp
@@ -1,23 +1,71 @@
 #include <Arduino.h>
-#define RX 44  // Board RX (connected to sensor TX)
-#define TX 43  // Board TX (connected to sensor RX)
+#include <SoftwareSerial.h>
 
-HardwareSerial WaveSerial(1);
+#define RX 8
+#define TX 9
+
+SoftwareSerial WaveSerial(RX, TX);
+
+// Presence tracking
+bool previousPresence = false;
+bool currentPresence = false;
+unsigned long lastDetectionTime = 0;
+int presenceCount = 0;
+
 
 void setup() 
 {
   Serial.begin(115200);
-  delay(1000); // Give USB serial time to initialize
-  Serial.println("Booting...");
-  WaveSerial.begin(115200, SERIAL_8N1, RX, TX);
-  Serial.println("Sensor UART ready");
+  WaveSerial.begin(115200);
+  Serial.println("Starting presence counter...");
 }
+
 
 void loop() 
 {
-  while (WaveSerial.available()) 
+  checkSensorData();
+  updatePresenceState();
+  detectNewPerson();
+}
+
+// ---- Function to read sensor data ----
+void checkSensorData() 
+{
+  if (WaveSerial.available()) 
   {
-    Serial.write(WaveSerial.read());
+    // Read the data (we don’t care what it is)
+    WaveSerial.read();
+
+    // Presence is assumed when data is received
+    currentPresence = true;
+    lastDetectionTime = millis();
   }
 }
+
+// ---- Function to update presence status based on time ----
+void updatePresenceState() 
+{
+  if (millis() - lastDetectionTime > 500) 
+  {
+    currentPresence = false;
+  }
+}
+
+// ---- Function to detect new person (rising edge) ----
+void detectNewPerson() 
+{
+  if (currentPresence && !previousPresence) 
+  {
+    presenceCount++;
+    Serial.print("New person detected! Total count: ");
+    Serial.println(presenceCount);
+  }
+
+  // Update previous state for next loop
+  previousPresence = currentPresence;
+}
+
+
+
+
 
