89085a95e6eb75498b9adb04d0dabcc7c5d00aa5
Erick-R777 <elsbr300@gmail.com>
2025-06-11 04:36:27 +0200

    add librarys plus finised the code for the light sensor

--- Quest_code/src/main.cpp
+++ Quest_code/src/main.cpp
@@ -1,16 +1,56 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <BH1750.h>
 
-
-
+BH1750 lightSensor;
 
 void setup() 
 {
+  Serial.begin(115200);
+  Wire.begin();
 
+  if (lightSensor.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) 
+  {
+    Serial.println("BH1750 gestart...");
+  } else 
+  {
+    Serial.println("Kon BH1750 niet starten. Controleer de bekabeling.");
+    while (1);
+  }
 }
 
 void loop() 
 {
-
- 
+  float lux = readLightLevel();
+  printLightCategory(lux);
+  delay(1000);  // Wacht 1 seconde tussen metingen
 }
 
+// Leest lux-waarde van sensor
+float readLightLevel() 
+{
+  float lux = lightSensor.readLightLevel();
+  Serial.print("Lichtintensiteit: ");
+  Serial.print(lux);
+  Serial.println(" lux");
+  return lux;
+}
+
+// Categoriseert de lichtsterkte
+void printLightCategory(float lux) 
+{
+  String category;
+
+  if (lux < 50) 
+  {
+    category = " Donker";
+  } else if (lux < 1000) {
+
+    category = " Normaal licht";
+  } else 
+  {
+    category = " Fel licht";
+  }
+
+  Serial.println("Lichtcategorie: " + category);
+}
